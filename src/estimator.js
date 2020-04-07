23b280016a2e3671920ed7dace3b671919c7b7af
charliekaks <kaksunchained@gmail.com>
2020-04-07 21:41:00 +0300

    correcting the styles for the code

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,19 @@
-const covid19ImpactEstimator = (data) => {
-    let impact ={};
-    let severeImpact = {};
+const covid19ImpactEstimator = data => {
+  let impact = {};
+  let severeImpact = {};
 
-    let currentlyInfectedImpact = data.reportedCases * 10;
-    impact.currentlyInfected = currentlyInfectedImpact;
+  const currentlyInfectedImpact = data.reportedCases * 10;
+  impact.currentlyInfected = currentlyInfectedImpact;
 
-    let currentlyInfectedSevere = data.reportedCases * 50;
-    severeImpact.currentlyInfected = currentlyInfectedSevere;
-    return{
-        data,
-        impact,
-        severeImpact
-    }
-    
+  const currentlyInfectedSevere = data.reportedCases * 50;
+  severeImpact.currentlyInfected = currentlyInfectedSevere;
+
+  
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
