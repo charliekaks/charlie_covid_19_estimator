549e07b82ecd3d2fa8897091310a5e5f4830d3da
charliekaks <kaksunchained@gmail.com>
2020-04-07 21:17:38 +0300

    Testing challenge one code base

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    let impact ={};
+    let severeImpact = {};
+
+    let currentlyInfectedImpact = data.reportedCases * 10;
+    impact.currentlyInfected = currentlyInfectedImpact;
+
+    let currentlyInfectedSevere = data.reportedCases * 50;
+    severeImpact.currentlyInfected = currentlyInfectedSevere;
+    return{
+        data,
+        impact,
+        severeImpact
+    }
+    
+};
 
 export default covid19ImpactEstimator;
