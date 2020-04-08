8ba34c9d152ddedbb0d784674086bc8bfc564fdc
charliekaks <kaksunchained@gmail.com>
2020-04-08 12:59:35 +0300

    Completing the test for infections by requested time

--- src/estimator.js
+++ src/estimator.js
@@ -8,6 +8,26 @@ const covid19ImpactEstimator = (data) => {
   const currentlyInfectedSevere = data.reportedCases * 50;
   severeImpact.currentlyInfected = currentlyInfectedSevere;
 
+  if(data.periodType === "days"){
+    const infectionsByRequestedTimeImpact = currentlyInfectedImpact * (2*Math.floor( data.timeToElapse/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
+
+    const infectionsByRequestedTimeSevereImpact = currentlyInfectedSevere * (2*Math.floor( data.timeToElapse/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeSevereImpact;
+  }else if(data.periodType === "weeks"){
+    const infectionsByRequestedTimeImpact = currentlyInfectedImpact * (2*Math.floor( (data.timeToElapse * 7)/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
+
+    const infectionsByRequestedTimeSevereImpact = currentlyInfectedSevere * (2*Math.floor( (data.timeToElapse*7)/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeSevereImpact;
+  }else if(data.periodType === "months"){
+    const infectionsByRequestedTimeImpact = currentlyInfectedImpact * (2*Math.floor( (data.timeToElapse*30)/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
+
+    const infectionsByRequestedTimeSevereImpact = currentlyInfectedSevere * (2*Math.floor( (data.timeToElapse * 30)/3));
+    impact.infectionsByRequestedTime = infectionsByRequestedTimeSevereImpact;
+  }
+
   return {
     data,
     impact,
