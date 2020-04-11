04ac90f6caf0b1142f44d55bcd5b2060e3f17e27
charliekaks <kaksunchained@gmail.com>
2020-04-11 13:47:53 +0300

    Changed all the round functions to trunc functions

--- src/estimator.js
+++ src/estimator.js
@@ -25,26 +25,26 @@ const covid19ImpactEstimator = (data) => {
 
   impact.infectionsByRequestedTime = infectionsOverTime(data, currentlyInfectedImpact);
   severeImpact.infectionsByRequestedTime = infectionsOverTime(data, currentlyInfectedSevere);
-  const severeCasesImpact = Math.round(0.15 * impact.infectionsByRequestedTime);
-  const severeCasesSevere = Math.round(0.15 * severeImpact.infectionsByRequestedTime);
+  const severeCasesImpact = Math.trunc(0.15 * impact.infectionsByRequestedTime);
+  const severeCasesSevere = Math.trunc(0.15 * severeImpact.infectionsByRequestedTime);
 
   impact.severeCasesByRequestedTime = severeCasesImpact;
   severeImpact.severeCasesByRequestedTime = severeCasesSevere;
 
-  const availableBedsImpact = Math.round(0.35 * data.totalHospitalBeds) - severeCasesImpact;
-  const availableBedsSevere = Math.round(0.35 * data.totalHospitalBeds) - severeCasesSevere;
+  const availableBedsImpact = Math.trunc(0.35 * data.totalHospitalBeds) - severeCasesImpact;
+  const availableBedsSevere = Math.trunc(0.35 * data.totalHospitalBeds) - severeCasesSevere;
 
   impact.hospitalBedsByRequestedTime = availableBedsImpact;
   severeImpact.hospitalBedsByRequestedTime = availableBedsSevere;
 
-  const impactICUPatients = Math.round(0.05 * impact.infectionsByRequestedTime);
-  const severeICUPatients = Math.round(0.05 * severeImpact.infectionsByRequestedTime);
+  const impactICUPatients = Math.trunc(0.05 * impact.infectionsByRequestedTime);
+  const severeICUPatients = Math.trunc(0.05 * severeImpact.infectionsByRequestedTime);
 
   impact.casesForICUByRequestedTime = impactICUPatients;
   severeImpact.casesForICUByRequestedTime = severeICUPatients;
 
-  const impactVentilators = Math.round(0.02 * impact.infectionsByRequestedTime);
-  const severeVentilators = Math.round(0.02 * severeImpact.infectionsByRequestedTime);
+  const impactVentilators = Math.trunc(0.02 * impact.infectionsByRequestedTime);
+  const severeVentilators = Math.trunc(0.02 * severeImpact.infectionsByRequestedTime);
 
   impact.casesForVentilatorsByRequestedTime = impactVentilators;
   severeImpact.casesForICUByRequestedTime = severeVentilators;
